9a2c9370c10641a86fcb8ceb305606c3417796c0
nkalvi <nkalvi@users.noreply.github.com>
2022-08-03 11:15:32 -0700

    Initial Commit

---
+++ Date Planner-710387/Date_Planner_710387App.swift
@@ -0,0 +1,17 @@
+//
+//  Date_Planner_710387App.swift
+//  Date Planner-710387
+//
+//  Created by Kana Nadarajan on 2022-08-03.
+//
+
+import SwiftUI
+
+@main
+struct Date_Planner_710387App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
